245884e73002038fcc2bc84cfbe052a679f21b82
Tony Lefebvre <tony.lefebvre@outsystems.com>
2026-07-01 17:38:28 +0200

    Add Android unavailable annotation method

--- android/src/main/java/com/outsystems/labs/capacitor/plugins/photoannotator/PhotoAnnotatorPlugin.java
+++ android/src/main/java/com/outsystems/labs/capacitor/plugins/photoannotator/PhotoAnnotatorPlugin.java
@@ -1,8 +1,15 @@
 package com.outsystems.labs.capacitor.plugins.photoannotator;
 
 import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "PhotoAnnotator")
 public class PhotoAnnotatorPlugin extends Plugin {
+
+    @PluginMethod
+    public void annotatePhoto(PluginCall call) {
+        call.unavailable("Photo annotation is only available on iOS.");
+    }
 }
